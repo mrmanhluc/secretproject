5e797a48740f8848a721d9fb41aeafa25dac17be
Nguyen Manh Luc (DT.DSG) <LucNM@fsoft.com.vn>
2018-11-06 11:31:22 +0900

    Add new version run without Jupyter notebook

---
+++ commond.py
@@ -0,0 +1,15 @@
+import datetime
+
+# Variance used to defined request url
+JOURNEY_TYPE = 'one-way'
+DEPARTURE = 'NRT'
+DESTINATION = 'SGN'
+LOCALE = 'en-US'
+PASSENGER = 1
+FROM_DATE = '12-01-2018'
+TO_DATE = '12-30-2018'
+
+# Variance used to selenium function
+CURRENT_TIME = datetime.datetime.now()
+CHROME_DRIVER = r'D:\02_Python\18_PullingData_HTML\chormdriver\chromedriver.exe'
+WAITING_TIME = 20
